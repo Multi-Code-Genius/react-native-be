2a38a46911b9e4186301de8d76f2f0c915790e48
jay rajshakha <jayrajshakha73@gmail.com>
2025-04-11 17:38:43 +0530

    fixed some issued

--- src/types/express.ts
+++ src/types/express.ts
@@ -1,4 +1,5 @@
-import { Request } from "express";
+import "express";
+
 declare global {
   namespace Express {
     interface Request {
@@ -11,5 +12,3 @@ declare global {
     }
   }
 }
-
-export {};
